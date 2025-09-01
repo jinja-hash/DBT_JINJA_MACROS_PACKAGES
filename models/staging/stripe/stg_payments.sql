3faac452631928d4002cd0aa4768d7ada2b70ff4
Adrian Torres Roda <jinjagit@gmail.com>
2025-09-01 15:49:12 +0000

    FIRST_MACRO

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -7,7 +7,7 @@ payments as (
         orderid as order_id,
         paymentmethod as payment_method,
         status as payment_status,
-        amount as payment_amount,
+        {{ cents_to_dollars() }} as payment_amount,
         created
     from
         {{source('stripe','payments')}} 
